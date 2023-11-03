a2b5e494330cd31b7bb590733d542fd2bb70e0ac
itamarpambo <itamarpambo751@gmail.com>
2023-11-03 14:00:45 +0100

    Voltando ao padding para incluir a página toda (dimensionar os espaçamentos em todos os lados)

--- lib/login.dart
+++ lib/login.dart
@@ -14,43 +14,45 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 60,),
-            Text(
-              "Sign Up to Misterminds",
-              style: TextStyle(
-                fontSize: 19,
-                fontWeight: FontWeight.w700
+        child: Padding(
+          padding: EdgeInsets.only(top: 60, left: 24, right: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Sign Up to Misterminds",
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.w700
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(right: 5), 
-                    child: Text(
-                      "Already have an account?",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black26
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 5), 
+                      child: Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black26
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.blueAccent
-                    ),
-                  )
-                ],
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.blueAccent
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ) 
     );
